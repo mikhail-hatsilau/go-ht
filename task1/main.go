18dfaef964107f571d17e7bcf9a3dedf87b8d368
Dzmitry Ryzhyk <bavarets@tut.by>
2019-02-24 11:49:36 +0300

    ololo

--- task1/main.go
+++ task1/main.go
@@ -1,6 +1,16 @@
 package main
 
-// Specify Filter function here
+func Filter(numbers []int, predicate func(item int, index int) bool) []int {
+	var result []int
+	
+	for i, number := range numbers {
+		if predicate(number, i) {
+			result = append(result, number)
+		}
+	}
+	
+	return result;
+}
 
 func main() {
 
