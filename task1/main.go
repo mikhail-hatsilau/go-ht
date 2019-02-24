e8d13bc6a16967df5a3f57c37df7bf048970a220
Tatsianenka Ilya <itiv422@gmail.com>
2019-02-24 19:22:22 +0300

    task1

--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,14 @@
 package main
 
-// Specify Filter function here
+func Filter(input []int, filter func(int, int) bool) []int {
+	var output []int
+	for index, value := range input {
+		if (filter(value, index)) {
+			output = append(output, value)
+		}
+	}
+	return output
+}
 
 func main() {
-
 }
