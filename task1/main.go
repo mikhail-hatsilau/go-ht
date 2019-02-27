d0247e87b7e14af619d616825ff10466cd23adc7
Pavelx4y16 <47992763+Pavelx4y16@users.noreply.github.com>
2019-02-27 16:29:23 +0300

    Update main.go
    First Task

--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,21 @@
 package main
 
-// Specify Filter function here
+import "fmt"
+
+func check(item int, index int) bool {
+	return item%2 == 0
+}
+
+func Filter(list []int, check func(int, int) bool) (result []int) {
+	for index, item := range list {
+		if check(item, index) {
+			result = append(result, item)
+		}
+	}
+	return
+}
 
 func main() {
-
+	input := []int{1, 3, 2, 4, 7, 6, 0}
+	fmt.Println(Filter(input, check))
 }
