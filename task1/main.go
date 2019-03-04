b599e59d77657ce3fe46b41212ad28eed57e8a32
Alexandr Michailov <mrmixal33@gmail.com>
2019-03-04 11:57:59 +0300

    Task1 complete

--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Specify Filter function here
+
+func Filter(array []int, predicate func(elem, _ int) bool) []int {
+	var result []int
+
+	for index, val := range array {
+		if predicate(val, index) == true {
+			result = append(result, val)
+		}
+	}
+	return result
+}
 
 func main() {
 
+	fmt.Println(Filter([]int{1, 2, 3, 4, 5, 6, 7}, func(elem, _ int) bool { return elem > 0 }))
 }
