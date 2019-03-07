fc1da8a2246dfc165c276238724d2bd9480b72fd
MaksimYudenko <yudenkomaksim@gmail.com>
2019-03-07 12:23:59 +0300

    Task2: done.

--- task1/main.go
+++ task1/main.go
@@ -1,20 +1,7 @@
 package main
 
-import "fmt"
-
-type predicate func(int, int) bool
-
-func Filter(slice []int, function predicate) []int {
-	var result []int
-	for i, item := range slice {
-		if function(item, i) == true {
-			result = append(result, item)
-		}
-	}
-	return result
-}
+// Specify Filter function here
 
 func main() {
-	fmt.Println("Even", Filter([]int{1, 2, 3, 4, 5},
-		func(item, index int) bool { return item%2 == 0 }))
+
 }
