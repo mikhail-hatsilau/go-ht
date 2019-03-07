f2f7611e3ffe1fd726e9ae8a2734cb4838115d50
Vasily <gorunov8720913@gmail.com>
2019-03-08 00:22:13 +0300

    task2 is done

--- task2/main.go
+++ task2/main.go
@@ -1,9 +1,32 @@
 package main
 
-// Specify MapTo function here
+func MapTo(slice []int, modify func(elem, index int) string) (result []string) {
+	for index, elem := range slice {
+		elem := modify(elem, index)
+		result = append(result, elem)
+	}
+	return
+}
+
+var intToStr = map[int]string{
+	1: "one",
+	2: "two",
+	3: "three",
+	4: "four",
+	5: "five",
+	6: "six",
+	7: "seven",
+	8: "eight",
+	9: "nine",
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	return MapTo(arr, func(elem, index int) string {
+		if elem, ok := intToStr[elem]; ok {
+			return elem
+		}
+		return "unknown"
+	})
 }
 
 func main() {
